4b577a8429d3ed834ef3115a291be227f16d4c01
Yarrow <yarrow@example.com>
2025-11-26 08:44:31 +0800

    Move Keil scripts to dedicated directory
    Add ARM ACLE and architecture flags to clangd config.

--- .zed/keil/clangd_config.bat
+++ .zed/keil/clangd_config.bat
@@ -59,7 +59,7 @@ if exist ".clangd" (
     @REM 创建配置文件内容
     @(
         echo CompileFlags:
-        echo   Add: [-include, stdint.h, -include, string.h, -IFirmware/CMSIS/Include, -ID:\Program Files\ARM\MDK5\ARM\ARMCLANG\include, -Wno-include-next]
+        echo   Add: [-include, stdint.h, -include, string.h, -IFirmware/CMSIS/Include, -ID:\Program Files\ARM\MDK5\ARM\ARMCLANG\include, -Wno-include-next,-D__ARM_ACLE=200, -march=armv8-m.main]
         echo Diagnostics:
         echo   Suppress: [unused-includes]
         echo Index:
